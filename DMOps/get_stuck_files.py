94a985b1348ecd0c8f28be53e2cdcb2bd9f84cf6
emcgrady <emcgrady92@gmail.com>
2022-07-26 01:43:39 +0200

    Fixed dataset bug

--- DMOps/get_stuck_files.py
+++ DMOps/get_stuck_files.py
@@ -15,15 +15,22 @@ def get_stuck_files(args):
     rule_id = args.rule_id
     temp = client.get_replication_rule(rule_id)
     df = pd.DataFrame(client.list_content(temp['scope'], temp['name']))
+    datasets = pd.DataFrame()
     final = pd.DataFrame()
-    for i in range(len(df['scope'])):
+    for i in range(len(df)):
         if df['name'].str.endswith('.file0001')[i]:
-            final = pd.concat([final, df['name']], ignore_index=True)
+            datasets = pd.concat([datasets, df['name']], ignore_index=True)
         else:
             temp = pd.DataFrame(client.list_dataset_replicas(df['scope'][i], df['name'][i]))
             temp = temp.loc[temp['state'] != 'AVAILABLE']
             if len(temp != 0):
-                final = pd.concat([final, temp['name']], ignore_index=True)
+                datasets = datasets.append(temp)
+    datasets = datasets.reset_index()
+    for i in range(len(datasets)):
+        temp = pd.DataFrame(client.list_content(datasets['scope'][i], datasets['name'][i]))
+        final = final.append(temp)
+    final = final.reset_index()
+    final = final['name']
     final.to_csv(args.file_name, header=None, index=None, sep=' ')
     
 if __name__ == '__main__':
