51df6faf00121169c1e94111df0f5d48fb9af67e
emcgrady <emcgrady92@gmail.com>
2022-09-07 14:42:41 +0200

    is_corrupt.py code cleanup

--- DMOps/is_corrupt.py
+++ DMOps/is_corrupt.py
@@ -1,81 +1,89 @@
-from __future__ import division, print_function
 from rucio.client.client import Client
+from pathlib import Path
 import pandas as pd
 import gfal2
 import argparse
-import subprocess
-import os
 
 class ArgumentParser():
     def __init__(self):
         self.parser = argparse.ArgumentParser(prog='is_corrupt')
-        self.parser.add_argument('--filename', '-f', help="specify filename as <scope:file>", action='store', dest='filename')
+        self.parser.add_argument('filename', help='specify filename as <scope:file>', action='store')
+
         
-def is_corrupt(args):
-    client = Client()
+def get_replicas(args):
     dids=[]
     scope, name = args.filename.split(':')
     dids.append({'scope':scope,'name':name})
-    
     df = pd.DataFrame(client.list_replicas(dids, all_states=True))
     checksum = df['adler32'][0]
-    
+    filesize = df['bytes'][0]
     rse = []
     state = []
-    
     for i, replica  in enumerate(list(df['pfns'][0].keys())):
         rse.append(df['pfns'][0][replica]['rse'])
         state.append(df['states'][0][rse[i]])
-        
     dit = {'replica':list(df['pfns'][0].keys()),
            'RSE': rse,
            'state': state,
           }
-    
     df = pd.DataFrame(dit)
+    end = len(df.columns)
+    df.insert(end, 'reason', '')
+    df.insert(end, 'diag', '')
     
-    ctxt = gfal2.creat_context()
-    
-    diag = {'diag': [], 'reason':[]}
-
-    for i in df.index:
-        try:
-            ctxt.checksum(df['replica'][i],'adler32')
-            print('Replica found on ' +  df['RSE'][i]) 
-            print('Check sum: ' + ctxt.checksum(df['replica'][i],'adler32'))
-            if checksum == ctxt.checksum(df['replica'][i],'adler32'):
-                print('Checksum is correct!')
-                diag['diag'].append('none')
-                diag['reason'].append('replica is ok')
-            else:
-                print('ERROR! Checksum incorrect!')
-                print('Expected ' + str(checksum) + ' and got ' + str(ctxt.checksum(df['replica'][i],'adler32')))
-                diag['diag'].append('replica corrupt')
-                diag['reason'].append('replica checksum mismatch at RSE')
-        except:
-            print('File not found on ' + df['RSE'][i])
-            diag['diag'].append('replica missing')
-            diag['reason'].append('replica not found')
-            
+    return df, filesize, checksum
         
-    df['diag'] = diag['diag']
-    df['reasoning'] = diag['reason']
-    
-    for i in df.loc[df['diag'] == 'none'].index:
-        file = df['replica'][i]
-        os.system('gfal-copy ' + file + ' /tmp/temp.root')
-        test = subprocess.check_output('gfal-sum /tmp/temp.root adler32', shell=True)
-        if str(test).split()[1][:8] == checksum:
-            print('Replica from ' + df['RSE'][i] + ' is not corrupt')
+def check(replica, rse, filesize, checksum):
+    try:
+        ctxt.checksum(replica,'adler32')
+        if filesize == ctxt.stat(replica).st_size:
+            if checksum == ctxt.checksum(replica,'adler32'):
+                diag = 'ok'
+                reason = 'file passed all tests'
+            else:
+                print('Checksum incorrect!')
+                print('Expected ' + str(checksum) + ' and got ' + str(ctxt.checksum(replica,'adler32')))
+                diag = 'file corrupt'
+                reason = 'file checksum mismatch'
         else:
-            print('Replica from ' + df['RSE'][i] + ' is corrupt!')
-            df['diag'][i] = 'replica corrupt'
-            df['reason'][i] = 'file checksum mismatch after local copy'
-        os.system('rm /tmp/temp.root')
+            print('Filesize incorrect!')
+            print('Expected ' + str(filesize) + ' and got ' + str(ctxt.stat(replica).st_size))
+            diag = 'file corrupt'
+            reason = 'filesize mismatch'
+    except:
+        print('File not found on ' + rse)
+        diag = 'file missing'
+        reason = 'file not found'
+        
+    return diag, reason
+        
+        
+def is_corrupt(args):
+    df, filesize, checksum = get_replicas(args)
+    print('Running remote tests...')
+    for i in df.index:
+        df['diag'][i], df['reason'][i] = check(df['replica'][i], df['RSE'][i], filesize, checksum)
+    
+    print('Copying files and running tests locally...')
+    p = Path('/tmp/temp.root')
+    
+    for i in df.loc[df['diag'] == 'ok'].index:
+        try:
+            t = ctxt.transfer_parameters()
+            ctxt.filecopy(t, df['replica'][i], 'file://' + str(p))
+            df['diag'][i], df['reason'][i] = check('file://' + str(p), df['RSE'][i], filesize, checksum)
+            p.unlink()
+        except: 
+            print('Replica from ' + df['RSE'][i] + ' failed to copy!')
+            df['diag'][i] = 'unknown'
+            df['reason'][i] = 'disk copy unavailable for copy but replica passed remote checksum and filesize checks'
 
     print(df)
 
 if __name__ == '__main__':
+    print('Be assured that the code is running')
     optmgr = ArgumentParser()
     args = optmgr.parser.parse_args()
+    ctxt = gfal2.creat_context()
+    client = Client()
     is_corrupt(args)
